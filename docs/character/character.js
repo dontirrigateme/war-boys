c898fb51f33b54cc9d3f6a63b9ca43e2c83022c0
dontirrigateme <smokey.alley44@gmail.com>
2025-11-09 05:18:10 -0500

    Update character.js

--- docs/character/character.js
+++ docs/character/character.js
@@ -33,6 +33,6 @@
 
 function charImg(cmd) {
   const slug = String(cmd).toLowerCase().replace(/\s+/g, "_");
-  return `../assets/characters/${slug}.webp`;
+  return `../assets/images/${slug}.webp`;
 }
 

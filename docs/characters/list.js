9a5405406a7a30ad22c5793dc1371311d6e1c0f5
dontirrigateme <smokey.alley44@gmail.com>
2025-11-17 05:34:20 -0500

    Update DATA_VERSION to 2025-11-09-10

--- docs/characters/list.js
+++ docs/characters/list.js
@@ -1,5 +1,5 @@
 (function(){
-  const DATA_VERSION = "2025-11-09-09";
+  const DATA_VERSION = "2025-11-09-10";
   const DATA_URL = `../data/characters.json?v=${DATA_VERSION}`;
   const IMG_BASE = "../assets/images/";
 

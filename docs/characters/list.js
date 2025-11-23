bdc4c92e98b48ac35e0360d412fab4e10b464d0a
dontirrigateme <smokey.alley44@gmail.com>
2025-11-23 08:29:52 -0500

    Update list.js

--- docs/characters/list.js
+++ docs/characters/list.js
@@ -1,5 +1,5 @@
 (function(){
-  const DATA_VERSION = "2025-11-09-15";
+  const DATA_VERSION = "2025-11-09-16";
   const DATA_URL = `../data/characters.json?v=${DATA_VERSION}`;
   const IMG_BASE = "../assets/images/";
 
